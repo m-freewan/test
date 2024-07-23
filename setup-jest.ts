0973e2bd3016e30d0692cbe6eacffe330a0d6ea9
m-freewan <mohammad.freewan@optimumpartners.co>
2024-07-23 11:39:27 +0300

    update changes

---
+++ setup-jest.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/+(*.)+(spec).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageReporters: ['html', 'lcov', 'text-summary'],
+};
